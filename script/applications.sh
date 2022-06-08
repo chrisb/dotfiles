dca2733b2c3b16b8ee4e301a70dc9254aea9be34
Chris Bielinski <chrisb@users.noreply.github.com>
2022-06-08 14:02:46 -0700

    fix symlinks

--- script/applications.sh
+++ script/applications.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "[apps] Copying configs"
-ln -sf applications/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-ln -sf applications/zsh/.zshrc ~/.zshrc
-ln -sf applications/spotify/com.spotify.client.plist ~/Library/Preferences/com.spotify.client.plist
+ln -sf "$PWD/applications/iterm/com.googlecode.iterm2.plist" ~/Library/Preferences/com.googlecode.iterm2.plist
+ln -sf "$PWD/applications/zsh/.zshrc" ~/.zshrc
+ln -sf "$PWD/applications/spotify/com.spotify.client.plist" ~/Library/Preferences/com.spotify.client.plist
